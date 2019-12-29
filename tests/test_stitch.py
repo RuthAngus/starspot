03fa495d0b5f23a49ca85ba2e1e75b201fa1e41f
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2019-12-29 14:24:54 +0000

    adding stdev as argument

--- tests/test_stitch.py
+++ tests/test_stitch.py
@@ -11,22 +11,51 @@ def test_stitching():
     y = np.zeros_like(t) + np.random.randn(N)*.1
     yerr = np.ones_like(y)*.1
 
+    # 3 gaps
     gap_times, steps = [5, 12, 17], [1, 2, 1]
     mu = sps.step_model(t, gap_times, steps)
     y += mu
 
-    plt.subplot(2, 1, 1)
-    plt.errorbar(t, y, yerr=yerr, fmt="k.")
-
-    star = sps.StitchModel(t, y, yerr, steps, gap_times)
+    star = sps.StitchModel(t, y, yerr, gap_times, steps, 2.0)
     star.model_offsets()
     map_soln = star.find_optimum()
     mu, var = star.evaluate_model(t)
-    plt.plot(t, mu)
 
-    plt.subplot(2, 1, 2)
-    plt.errorbar(t, y - mu, yerr=yerr, fmt="k.")
-    plt.savefig("test_stitch")
+    # plt.subplot(2, 1, 1)
+    # plt.errorbar(t, y, yerr=yerr, fmt="k.")
+    # plt.plot(t, mu)
+    # plt.subplot(2, 1, 2)
+    # plt.errorbar(t, y - mu, yerr=yerr, fmt="k.")
+    # plt.savefig("test_stitch")
+
+    print(map_soln["step1"], map_soln["step2"], map_soln["step3"])
+    assert np.isclose(map_soln["step1"], 1, atol=.1)
+    assert np.isclose(map_soln["step2"], 2, atol=.1)
+    assert np.isclose(map_soln["step3"], 1, atol=.1)
+
+    # 13 gaps
+    # Simulate data
+    N = 500
+    t = np.linspace(0, 100, N)
+    y = np.zeros_like(t) + np.random.randn(N)*.1
+    yerr = np.ones_like(y)*.1
+
+    gap_times = [5, 12, 17, 20, 30, 35, 40, 45, 50, 55, 60, 70, 80]
+    steps = [1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1, 2, 1]
+    mu = sps.step_model(t, gap_times, steps)
+    y += mu
+
+    star = sps.StitchModel(t, y, yerr, gap_times, steps, 2.0)
+    star.model_offsets()
+    map_soln = star.find_optimum()
+    mu, var = star.evaluate_model(t)
+
+    # plt.subplot(2, 1, 1)
+    # plt.errorbar(t, y, yerr=yerr, fmt="k.")
+    # plt.plot(t, mu)
+    # plt.subplot(2, 1, 2)
+    # plt.errorbar(t, y - mu, yerr=yerr, fmt="k.")
+    # plt.savefig("test_stitch13")
 
     print(map_soln["step1"], map_soln["step2"], map_soln["step3"])
     assert np.isclose(map_soln["step1"], 1, atol=.1)
