05e3387437d4a8e54f91e20cefb94cdbda8f0bef
Ruth Angus <ruth.angus@astro.ox.ac.uk>
2019-12-29 14:24:37 +0000

    adding stdev as argument

--- starspot/stitch.py
+++ starspot/stitch.py
@@ -21,16 +21,19 @@ class StitchModel(object):
         gap_times (array): The times of the gaps between light curves.
         steps (array): The flux differences between the 1st light curve, and
             all following light curves.
+        stdev (Optional[float]): The standard deviation of the Gaussian prior
+            for flux differences between light curves.
 
     """
 
-    def __init__(self, t, y, yerr, steps, gap_times):
+    def __init__(self, t, y, yerr, gap_times, steps, stdev):
 
         self.t = t
         self.y = y
         self.yerr = yerr
         self.steps = steps
         self.gap_times = gap_times
+        self.stdev = stdev
 
     def model_offsets(self):
         """
@@ -38,6 +41,8 @@ class StitchModel(object):
 
         """
 
+        stdev = self.stdev
+        # stdev = 2.0
         nsteps = len(self.steps)
         with pm.Model() as model:
 
@@ -46,14 +51,55 @@ class StitchModel(object):
             logrho = pm.Normal("logrho", mu=0.0, sd=5.0)
 
             # Define step variables
-            step1 = pm.Normal("step1", mu=self.steps[0], sd=2.0)
+            step1 = pm.Normal("step1", mu=self.steps[0], sd=stdev)
             steps = step1
             if nsteps > 1:
-                step2 = pm.Normal("step2", mu=self.steps[1], sd=2.0)
+                step2 = pm.Normal("step2", mu=self.steps[1], sd=stdev)
                 steps = [step1, step2]
             if nsteps > 2:
-                step3 = pm.Normal("step3", mu=self.steps[2], sd=2.0)
+                step3 = pm.Normal("step3", mu=self.steps[2], sd=stdev)
                 steps = [step1, step2, step3]
+            if nsteps > 3:
+                step4 = pm.Normal("step4", mu=self.steps[3], sd=stdev)
+                steps = [step1, step2, step3, step4]
+            if nsteps > 4:
+                step5 = pm.Normal("step5", mu=self.steps[4], sd=stdev)
+                steps = [step1, step2, step3, step4, step5]
+            if nsteps > 5:
+                step6 = pm.Normal("step6", mu=self.steps[5], sd=stdev)
+                steps = [step1, step2, step3, step4, step5, step6]
+            if nsteps > 6:
+                step7 = pm.Normal("step7", mu=self.steps[6], sd=stdev)
+                steps = [step1, step2, step3, step4, step5, step6, step7]
+            if nsteps > 7:
+                step8 = pm.Normal("step8", mu=self.steps[7], sd=stdev)
+                steps = [step1, step2, step3, step4, step5, step6, step7,
+                         step8]
+            if nsteps > 8:
+                step9 = pm.Normal("step9", mu=self.steps[8], sd=stdev)
+                steps = [step1, step2, step3, step4, step5, step6, step7,
+                         step8, step9]
+            if nsteps > 9:
+                step10 = pm.Normal("step10", mu=self.steps[9], sd=stdev)
+                steps = [step1, step2, step3, step4, step5, step6, step7,
+                         step8, step9, step10]
+            if nsteps > 10:
+                step11 = pm.Normal("step11", mu=self.steps[10], sd=stdev)
+                steps = [step1, step2, step3, step4, step5, step6, step7,
+                         step8, step9, step10, step11]
+            if nsteps > 11:
+                step12 = pm.Normal("step12", mu=self.steps[11], sd=stdev)
+                steps = [step1, step2, step3, step4, step5, step6, step7,
+                         step8, step9, step10, step11, step12]
+            if nsteps > 12:
+                step13 = pm.Normal("step13", mu=self.steps[12], sd=stdev)
+                steps = [step1, step2, step3, step4, step5, step6, step7,
+                         step8, step9, step10, step11, step12, step13]
+            if nsteps > 13:
+                step14 = pm.Normal("step14", mu=self.steps[13], sd=stdev)
+                steps = [step1, step2, step3, step4, step5, step6, step7,
+                         step8, step9, step10, step11, step12, step13, step14]
+
 
             # The step model
             mu = step_model(self.t, self.gap_times, steps)
